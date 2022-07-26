dea7bd13b025239974a2182690a7cd39b9aa96f4
Tibo Stroo <tibo.stroo@hotmail.be>
2022-07-26 17:31:37 +0200

    Begin using better shapes with dynamic style

--- src/SvgNode.js
+++ src/SvgNode.js
@@ -16,34 +16,78 @@ import triangle from "./assets/triangle.svg";
 import comunica from "./assets/comunica.svg";
 import rmlio from "./assets/rmlio.png";
 
+
+
+const SHAPES = {
+    "8-star": eightStar,
+    "big-star": bigStar,
+    "circle": circle,
+    "cylinder": cylinder,
+    "diamond": diamond,
+    "hexagon": hexagon,
+    "note": note,
+    "square": square,
+    "star": star,
+    "triangle": triangle,
+    "comunica": comunica,
+    "rmlio": rmlio
+};
+
+export function getShape(shapeId, fill="none", stroke="#000", strokeWidth=1) {
+    const SHAPES = {
+        "8-star":
+            <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+                    <path style={{fill:fill, fillRule:"evenodd", stroke:stroke,strokeWidth:strokeWidth,strokeDasharray:"none",paintOrder:"markers fill stroke"}} d="m28.78 35.452-8.39-.1-5.86 6.003-5.86-6.002-8.39.1.1-8.389-6.001-5.86L.38 15.341l-.1-8.388 8.388.1 5.861-6.002 5.86 6.002 8.39-.1-.1 8.388 6.001 5.861-6.002 5.861z" transform="matrix(1.22893 0 0 1.2302 7.144 -1.063)"/>
+                </svg>,
+
+        "big-star":
+            `<svg xml:space="preserve" width="50" height="50" xmlns="http://www.w3.org/2000/svg">
+                    <path style="fill:${fill};fill-rule:evenodd;stroke:${stroke};stroke-width:${strokeWidth};paint-order:markers fill stroke" d="m20.048 19.912-7.264-3.291-1.742 7.783-2.81-7.464-6.735 4.272 3.292-7.265-7.783-1.742 7.464-2.81L.198 2.662l7.265 3.291L9.204-1.83l2.81 7.464 6.735-4.272-3.291 7.265 7.783 1.742-7.465 2.81z" transform="matrix(1.7695 -.4742 .47414 1.76976 1.41 9.952)"/>
+                </svg>`,
+
+       // "circle":
+        //    <svg width="50mm" height="50mm" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+         //           <circle style="fill:${fill};fill-rule:evenodd;stroke:${stroke};stroke-width:${strokeWidth};stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:.4;stroke-dasharray:none;paint-order:markers fill stroke" cx="25" cy="25" r="25"/>
+          //      </svg>
+    }
+
+    return SHAPES[shapeId];
+}
+
+
 export default memo(({data, isConnectable}) => {
 
-    const SHAPES = {
-        "8-star": eightStar,
-        "big-star": bigStar,
-        "circle": circle,
-        "cylinder": cylinder,
-        "diamond": diamond,
-        "hexagon": hexagon,
-        "note": note,
-        "square": square,
-        "star": star,
-        "triangle": triangle,
-        "comunica": comunica,
-        "rmlio": rmlio
-    };
+
 
     //console.log(`Data van SvgNode is ${JSON.stringify(data)}`);
 
     //alert(JSON.stringify(data))
 
+
+
     let width = data["width"] || 50;
     let height = data["height"] || 50;
 
+    let element = getShape("8-star");
+   // console.log(element)
+
     return (
+
         <>
             <svg style={{width: width, height: height}}>
-                <image style={{width: width, height: height}} href={SHAPES[data.shape]}/>
+                getShape("8-star")
+                <svg style={{width: width}}>
+
+                    {
+                       element
+                    }
+
+                </svg>
+
+
+
+
+                {/*<image style={{width: width, height: height}} href={SHAPES[data.shape]}/>*/}
 
                 <text fontSize="12px"  /*x="50%" y="50%"*/ /*dominantBaseline="middle" textAnchor="middle"*/>
                     {data.label.split("\n").map((e, i) => {
