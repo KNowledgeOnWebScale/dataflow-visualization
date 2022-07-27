119ca78dbfa8b8f8f5b381cf9bc3f74eec17ad33
Tibo Stroo <tibo.stroo@hotmail.be>
2022-07-27 11:31:49 +0200

    fill, stroke and strokeWidth of nodes

--- src/SvgNode.js
+++ src/SvgNode.js
@@ -2,23 +2,11 @@ import React, {memo} from 'react';
 
 import {Handle} from 'react-flow-renderer';
 
-import eightStar from "./assets/8-star.svg";
-import bigStar from "./assets/big-star.svg";
-import circle from "./assets/circle.svg";
-import cylinder from "./assets/cylinder.svg";
-import diamond from "./assets/diamond.svg";
-import hexagon from "./assets/hexagon.svg";
-import note from "./assets/note.svg";
-import square from "./assets/square.svg";
-import star from "./assets/star.svg";
-import triangle from "./assets/triangle.svg";
-
 import comunica from "./assets/comunica.svg";
 import rmlio from "./assets/rmlio.png";
 
 
-
-const SHAPES = {
+/*const SHAPES = {
     "8-star": eightStar,
     "big-star": bigStar,
     "circle": circle,
@@ -31,24 +19,135 @@ const SHAPES = {
     "triangle": triangle,
     "comunica": comunica,
     "rmlio": rmlio
-};
+};*/
 
-export function getShape(shapeId, fill="none", stroke="#000", strokeWidth=1) {
+export function getShape(shapeId, fill, stroke, strokeWidth ) {
     const SHAPES = {
         "8-star":
             <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
-                    <path style={{fill:fill, fillRule:"evenodd", stroke:stroke,strokeWidth:strokeWidth,strokeDasharray:"none",paintOrder:"markers fill stroke"}} d="m28.78 35.452-8.39-.1-5.86 6.003-5.86-6.002-8.39.1.1-8.389-6.001-5.86L.38 15.341l-.1-8.388 8.388.1 5.861-6.002 5.86 6.002 8.39-.1-.1 8.388 6.001 5.861-6.002 5.861z" transform="matrix(1.22893 0 0 1.2302 7.144 -1.063)"/>
-                </svg>,
+                <path style={{
+                    fill: fill,
+                    fillRule: "evenodd",
+                    stroke: stroke,
+                    strokeWidth: strokeWidth,
+                    strokeDasharray: "none",
+                    paintOrder: "markers fill stroke"
+                }}
+                      d="m28.78 35.452-8.39-.1-5.86 6.003-5.86-6.002-8.39.1.1-8.389-6.001-5.86L.38 15.341l-.1-8.388 8.388.1 5.861-6.002 5.86 6.002 8.39-.1-.1 8.388 6.001 5.861-6.002 5.861z"
+                      transform="matrix(1.22893 0 0 1.2302 7.144 -1.063)"/>
+            </svg>,
 
         "big-star":
-            `<svg xml:space="preserve" width="50" height="50" xmlns="http://www.w3.org/2000/svg">
-                    <path style="fill:${fill};fill-rule:evenodd;stroke:${stroke};stroke-width:${strokeWidth};paint-order:markers fill stroke" d="m20.048 19.912-7.264-3.291-1.742 7.783-2.81-7.464-6.735 4.272 3.292-7.265-7.783-1.742 7.464-2.81L.198 2.662l7.265 3.291L9.204-1.83l2.81 7.464 6.735-4.272-3.291 7.265 7.783 1.742-7.465 2.81z" transform="matrix(1.7695 -.4742 .47414 1.76976 1.41 9.952)"/>
-                </svg>`,
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                <path style={{fill: fill, fillRule: "evenodd", stroke: stroke, paintOrder: "markers fill stroke"}}
+                      d="m20.048 19.912-7.264-3.291-1.742 7.783-2.81-7.464-6.735 4.272 3.292-7.265-7.783-1.742 7.464-2.81L.198 2.662l7.265 3.291L9.204-1.83l2.81 7.464 6.735-4.272-3.291 7.265 7.783 1.742-7.465 2.81z"
+                      transform="matrix(1.7695 -.4742 .47414 1.76976 1.41 9.952)"/>
+            </svg>,
 
-       // "circle":
-        //    <svg width="50mm" height="50mm" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
-         //           <circle style="fill:${fill};fill-rule:evenodd;stroke:${stroke};stroke-width:${strokeWidth};stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:.4;stroke-dasharray:none;paint-order:markers fill stroke" cx="25" cy="25" r="25"/>
-          //      </svg>
+        "circle":
+            <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+                <circle style={{
+                    fill: fill,
+                    fillRule: "evenodd",
+                    stroke: stroke,
+                    strokeWidth: strokeWidth,
+                    strokeLinecap: "round",
+                    strokeLinejoin: "round",
+                    strokeMiterlimit: 0.4,
+                    strokeDasharray: "none",
+                    paintOrder: "markers fill stroke"
+                }} cx="25" cy="25" r="25"/>
+            </svg>,
+
+        "cylinder":
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 50 50">
+                <g>
+                    <path style={{fill: fill,/* TODO: opacity:0.8,*/ stroke: stroke, strokeWidth: strokeWidth}}
+                          d="M25 6.897C11.669 6.897 0.862 5.353 0.862 3.448v43.103c0 1.904 10.807 3.448 24.138 3.448s24.138 -1.544 24.138 -3.448V3.448C49.138 5.353 38.331 6.897 25 6.897z"/>
+                    <path style={{stroke: stroke, fill: fill, strokeWidth: strokeWidth}} cx="29" cy="4" rx="28" ry="4"
+                          d="M49.138 3.448A24.138 3.448 0 0 1 25 6.897A24.138 3.448 0 0 1 0.862 3.448A24.138 3.448 0 0 1 49.138 3.448z"/>
+                    <path style={{fill: fill, stroke: stroke, strokeWidth: strokeWidth}} cx="29" cy="54" rx="28" ry="4"
+                          d="M49.138 46.552A24.138 3.448 0 0 1 25 50A24.138 3.448 0 0 1 0.862 46.552A24.138 3.448 0 0 1 49.138 46.552z"/>
+                </g>
+            </svg>,
+
+        "diamond":
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                <path style={{
+                    fill: fill,
+                    fillRule: "evenodd",
+                    stroke: stroke,
+                    strokeWidth: strokeWidth,
+                    strokeDasharray: "none",
+                    paintOrder: "markers fill stroke"
+                }} transform="rotate(44.999)" d="M18.025-17.331h34.663v34.663H18.025z"/>
+            </svg>,
+
+        "hexagon":
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                <path style={{
+                    fill: fill,
+                    fillRule: "evenodd",
+                    stroke: stroke,
+                    strokeWidth: strokeWidth,
+                    strokeDasharray: "none",
+                    paintOrder: "markers fill stroke"
+                }} d="M14.249 25.648H-6.29L-16.56 7.862-6.29-9.924h20.538L24.517 7.862z"
+                      transform="matrix(1.181 0 0 1.37277 20.3 14.253)"/>
+            </svg>,
+
+
+        "note":
+        // Eerste fill op none laten, want dat is background
+            <svg viewBox="6.579 6.579 50 50" xmlns="http://www.w3.org/2000/svg">
+                <path fill="none" d="M0 0h63.158v63.158H0z"/>
+                <path style={{stroke: stroke, fill: fill, strokeWidth: strokeWidth}}
+                      d="M55.263 42.105l-13.166 13.158H10.521A2.621 2.621 0 0 1 7.895 52.65V10.508C7.895 9.066 9.066 7.895 10.508 7.895h42.142c1.442 0 2.613 1.176 2.613 2.629V42.105z"/>
+            </svg>,
+
+        "square":
+            <svg viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+                <path style={{
+                    fill: fill,
+                    fillRule: "evenodd",
+                    stroke: stroke,
+                    strokeWidth: strokeWidth,
+                    strokeLinecap: "round",
+                    strokeLinejoin: "round",
+                    strokeMiterlimit: 0.4,
+                    strokeDasharray: "none",
+                    paintOrder: "markers fill stroke"
+                }} d="M0 0h49.5v49.5H0z"/>
+            </svg>,
+
+        "star":
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                <path style={{
+                    fill: fill,
+                    fillRule: "evenodd",
+                    stroke: stroke,
+                    strokeWidth: strokeWidth,
+                    strokeDasharray: "none",
+                    paintOrder: "markers fill stroke"
+                }}
+                      d="m19.25 21.139-6.74-3.952-6.628 4.14 1.675-7.633-5.985-5.025 7.777-.765L12.278.66l3.131 7.159 7.795.547-5.841 5.19z"
+                      transform="matrix(2.19417 0 0 2.30024 -2.178 -.121)"/>
+            </svg>,
+
+        "triangle":
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50">
+                <path style={{
+                    fill: fill,
+                    fillRule: "evenodd",
+                    stroke: stroke,
+                    strokeWidth: strokeWidth,
+                    strokeDasharray: "none",
+                    paintOrder: "markers fill stroke"
+                }} d="M19.419 52.828H-8.407L5.506 28.731z" transform="matrix(1.73845 0 0 2.00674 15.428 -56.539)"/>
+            </svg>,
+
+        "comunica": <image href={comunica} width="100%" height="100%" preserveAspectRatio="xMinYMin slice"/>,
+        "rmlio": <image href={rmlio} width="100%" height="100%" preserveAspectRatio="xMinYMin slice"/>
     }
 
     return SHAPES[shapeId];
@@ -57,35 +156,23 @@ export function getShape(shapeId, fill="none", stroke="#000", strokeWidth=1) {
 
 export default memo(({data, isConnectable}) => {
 
-
-
-    //console.log(`Data van SvgNode is ${JSON.stringify(data)}`);
-
-    //alert(JSON.stringify(data))
-
-
-
     let width = data["width"] || 50;
     let height = data["height"] || 50;
 
-    let element = getShape("8-star");
-   // console.log(element)
+    let element = getShape(data.shape || "square", data.fill || "white", data.stroke || "black", data.strokeWidth || 1);
+    // console.log(element)
 
     return (
-
         <>
             <svg style={{width: width, height: height}}>
                 getShape("8-star")
                 <svg style={{width: width}}>
 
                     {
-                       element
+                        element
                     }
 
                 </svg>
-
-
-
 
                 {/*<image style={{width: width, height: height}} href={SHAPES[data.shape]}/>*/}
 
@@ -95,7 +182,8 @@ export default memo(({data, isConnectable}) => {
                             return <tspan key={i} x="50%" dy="12px" dominantBaseline="middle"
                                           textAnchor="middle">{e}</tspan>
                         } else {
-                            return <tspan key={i} x="50%" y={50 - ((data.label.split("\n").length - 1) * height/12/2) + "%"}  // height/12, want fontsize is 12, TODO: dynamisch maken
+                            return <tspan key={i} x="50%"
+                                          y={50 - ((data.label.split("\n").length - 1) * height / 12 / 2) + "%"}  // height/12, want fontsize is 12, TODO: dynamisch maken
                                           dominantBaseline="middle" textAnchor="middle">{e}</tspan>
                         }
                     })}
--- src/data/exampleData4.js
+++ src/data/exampleData4.js
@@ -1,31 +1,31 @@
 import {MarkerType} from "react-flow-renderer";
 
 export const nodesData = [
-    {id: 'CSV', type: "custom", data: {label: 'CSV', shape: "cylinder"}, position: {x: 0, y: -30},},
-    {id: "JSON", type: "custom", data: {label: "JSON", shape: "cylinder"}, position: {x: 0, y: 30},},
-    {id: "XML", type: "custom", data: {label: "XML", shape: "cylinder"}, position: {x: 0, y: 90},},
-    {id: "MySQL", type: "custom", data: {label: "MySQL", shape: "cylinder"}, position: {x: 0, y: 150},},
-    {id: "API", type: "custom", data: {label: "API", shape: "square"}, position: {x: 0, y: 210},},
+    {id: 'CSV', type: "custom", data: {label: 'CSV', shape: "cylinder", fill:"indianred"}, position: {x: 0, y: -30},},
+    {id: "JSON", type: "custom", data: {label: "JSON", shape: "cylinder", fill:"lightcoral"}, position: {x: 0, y: 30},},
+    {id: "XML", type: "custom", data: {label: "XML", shape: "cylinder", fill:"sandybrown"}, position: {x: 0, y: 90},},
+    {id: "MySQL", type: "custom", data: {label: "MySQL", shape: "cylinder", fill:"khaki"}, position: {x: 0, y: 150},},
+    {id: "API", type: "custom", data: {label: "API", shape: "square", fill:"darkseagreen"}, position: {x: 0, y: 210},},
 
-    {id: "RML-CSV", type: "custom", data: {label: "RML\nTranslator", shape: "8-star"}, position: {x: 80, y: -30},},
-    {id: "RML-JSON", type: "custom", data: {label: "RML\nTranslator", shape: "8-star"}, position: {x: 80, y: 30},},
-    {id: "RML-XML", type: "custom", data: {label: "RML\nTranslator", shape: "8-star"}, position: {x: 80, y: 90},},
-    {id: "RML-MySQL", type: "custom", data: {label: "RML\nTranslator", shape: "8-star"}, position: {x: 80, y: 150},},
-    {id: "RML-API", style: {backgroundColor: "white"}, type: "custom", data: {label: "RML\nTranslator", shape: "8-star"}, position: {x: 80, y: 210},},
+    {id: "RML-CSV", type: "custom", data: {label: "RML\nTranslator", shape: "8-star", fill:"dodgerblue"}, position: {x: 80, y: -30},},
+    {id: "RML-JSON", type: "custom", data: {label: "RML\nTranslator", shape: "8-star", fill:"dodgerblue"}, position: {x: 80, y: 30},},
+    {id: "RML-XML", type: "custom", data: {label: "RML\nTranslator", shape: "8-star", fill:"dodgerblue"}, position: {x: 80, y: 90},},
+    {id: "RML-MySQL", type: "custom", data: {label: "RML\nTranslator", shape: "8-star", fill:"dodgerblue"}, position: {x: 80, y: 150},},
+    {id: "RML-API", /*style: {backgroundColor: "white"},*/ type: "custom", data: {label: "RML\nTranslator", shape: "8-star", fill:"dodgerblue"}, position: {x: 80, y: 210},},
 
-    {id: "RDF-CSV", type: "custom", data: {label: "RDF", shape: "cylinder", height: 30}, position: {x: 120, y: -20},},
-    {id: "RDF-JSON", type: "custom", data: {label: "RDF", shape: "cylinder", height: 30}, position: {x: 120, y: 40},},
-    {id: "RDF-XML", type: "custom", data: {label: "RDF", shape: "cylinder", height: 30}, position: {x: 120, y: 100},},
-    {id: "RDF-MySQL", type: "custom", data: {label: "RDF", shape: "cylinder", height: 30}, position: {x: 120, y: 160},},
-    {id: "RDF-API", type: "custom", data: {label: "RDF", shape: "cylinder", height: 30}, position: {x: 120, y: 220},},
+    {id: "RDF-CSV", type: "custom", data: {label: "RDF", shape: "cylinder", height: 30, fill:"lightgreen"}, position: {x: 130, y: -20},},
+    {id: "RDF-JSON", type: "custom", data: {label: "RDF", shape: "cylinder", height: 30, fill:"lightgreen"}, position: {x: 130, y: 40},},
+    {id: "RDF-XML", type: "custom", data: {label: "RDF", shape: "cylinder", height: 30, fill:"lightgreen"}, position: {x: 130, y: 100},},
+    {id: "RDF-MySQL", type: "custom", data: {label: "RDF", shape: "cylinder", height: 30, fill:"lightgreen"}, position: {x: 130, y: 160},},
+    {id: "RDF-API", type: "custom", data: {label: "RDF", shape: "cylinder", height: 30, fill:"lightgreen"}, position: {x: 130, y: 220},},
 
-    {id: "rmlio", type: "custom", data: {label: "rml.io", shape: "note"}, position: {x: 80, y: 280}},
+    {id: "rmlio", type: "custom", data: {label: "rml.io", shape: "note", fill:"steelblue"}, position: {x: 80, y: 280}},
 
     {id: 'comunica', type: 'custom', data: {label: '', shape: "comunica"}, position: {x: 600, y: 90},},
-    {id: "SPARQL", type: "custom", data: {label: "SPARQL", shape: "note"}, position: {x: 600, y: 180}},
+    {id: "SPARQL", type: "custom", data: {label: "SPARQL", shape: "note", fill:"steelblue"}, position: {x: 600, y: 180}},
 
-    {id: "note1", type: "custom", data: {label:"RMLTranslator translates raw\ndata to RDF on-the-fly,\nbased on the sub-queries\nof comunica", shape: "note", width: 150, height: 150}, position: {x: 200, y:-100}},
-    {id: "note2", style: {backgroundColor: "red"}, type: "custom", data: {label:"Comunica queries multiple\nRDF datasets SPARQL\nsub-queries goes in,\nresults come out", shape: "note", width: 150, height: 150}, position: {x: 450, y:-50}}
+    {id: "note1", type: "custom", data: {label:"RMLTranslator translates raw\ndata to RDF on-the-fly,\nbased on the sub-queries\nof comunica", shape: "note", width: 150, height: 150, fill:"lightyellow", strokeWidth:0.1, stroke:"khaki"}, position: {x: 200, y:-100}},
+    {id: "note2", /*style: {backgroundColor: "red"},*/ type: "custom", data: {label:"Comunica queries multiple\nRDF datasets SPARQL\nsub-queries goes in,\nresults come out", shape: "note", width: 150, height: 150, fill:"lightyellow", strokeWidth:0.1, stroke:"khaki"}, position: {x: 450, y:-50}}
 
 
 ];
