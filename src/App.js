4acfead6e2ca6b6998d10e5db6a175b33206d946
Tibo Stroo <tibo.stroo@hotmail.be>
2022-07-29 14:56:00 +0200

    Support for YAML

--- src/App.js
+++ src/App.js
@@ -11,25 +11,50 @@ import ReactFlow, {
 } from 'react-flow-renderer';
 
 //import CodeEditor from '@uiw/react-textarea-code-editor';
-import Editor, { DiffEditor, useMonaco, loader } from "@monaco-editor/react";
+import Editor, {DiffEditor, useMonaco, loader} from "@monaco-editor/react";
 
 
 // import CustomEdge from './CustomEdge';
 import SvgNode from "./SvgNode";
 
 // import {globalDefaultsJSON, nodesJSON, edgesJSON} from "./data"
-import {globalDefaultsJSON as globalDefaultsJSON1, nodesJSON as nodesJSON1 , edgesJSON as edgesJSON1} from "./data/exampleData1";
-import {globalDefaultsJSON as globalDefaultsJSON2, nodesJSON as nodesJSON2, edgesJSON as edgesJSON2} from "./data/exampleData2";
-import {globalDefaultsJSON as globalDefaultsJSON3, nodesJSON as nodesJSON3, edgesJSON as edgesJSON3} from "./data/exampleData3";
-import {globalDefaultsJSON as globalDefaultsJSON4, nodesJSON as nodesJSON4, edgesJSON as edgesJSON4} from "./data/exampleData4";
-import {globalDefaultsJSON as globalDefaultsJSON5, nodesJSON as nodesJSON5, edgesJSON as edgesJSON5} from "./data/exampleData5";
+import {
+    globalDefaultsJSON as globalDefaultsJSON1,
+    nodesJSON as nodesJSON1,
+    edgesJSON as edgesJSON1
+} from "./data/exampleData1";
+import {
+    globalDefaultsJSON as globalDefaultsJSON2,
+    nodesJSON as nodesJSON2,
+    edgesJSON as edgesJSON2
+} from "./data/exampleData2";
+import {
+    globalDefaultsJSON as globalDefaultsJSON3,
+    nodesJSON as nodesJSON3,
+    edgesJSON as edgesJSON3
+} from "./data/exampleData3";
+import {
+    globalDefaultsJSON as globalDefaultsJSON4,
+    nodesJSON as nodesJSON4,
+    edgesJSON as edgesJSON4
+} from "./data/exampleData4";
+import {
+    globalDefaultsJSON as globalDefaultsJSON5,
+    nodesJSON as nodesJSON5,
+    edgesJSON as edgesJSON5
+} from "./data/exampleData5";
 
 import {parseNodes, parseEdges, parseGlobalDefaults} from "./util";
-import {Button, FloatingLabel, Form} from "react-bootstrap";
+import {Button} from "react-bootstrap";
+import Dropdown from 'react-bootstrap/Dropdown';
+
+import YAML from "js-yaml";
+
 
 //const edgeTypes = {
 //  custom: CustomEdge,
 //};
+
 
 const nodeTypes = {
     custom: SvgNode
@@ -51,19 +76,72 @@ const EdgesFlow = () => {
     const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
 
 
+    const [language, setLanguage] = useState("json");
+
     const [globalDefaults, setGlobalDefaults] = useState(JSON.stringify({}));
     const [nodesData, setNodesData] = useState(JSON.stringify([]));
     const [edgesData, setEdgesData] = useState(JSON.stringify([]));
 
+
+    function json2yaml(jsonData) {
+        let yamlValue;
+        try {
+            yamlValue = YAML.dump(JSON.parse(jsonData));
+            return yamlValue;
+        } catch (e) {
+            return e;
+        }
+    }
+
+    function yaml2json(yamlData) {
+
+        let jsonValue;
+        try {
+            jsonValue = JSON.stringify(YAML.load(yamlData.toString()));
+            return jsonValue;
+        } catch (e) {
+            return e;
+        }
+    }
+
+    function changeLanguage(e) {
+
+        let newLang = e;
+
+        if (newLang === "yaml" && language === "json") {
+            setGlobalDefaults(json2yaml(globalDefaults));
+            setNodesData(json2yaml(nodesData));
+            setEdgesData(json2yaml(edgesData));
+            setLanguage(e);
+        } else if (newLang === "json" && language === "yaml") {
+            setGlobalDefaults(yaml2json(globalDefaults));
+            setNodesData(yaml2json(nodesData));
+            setEdgesData(yaml2json(edgesData));
+            setLanguage(e);
+        }
+
+    }
+
+    // Convert what's inside the editors to a graph
     function handleConvert(e) {
         e.preventDefault();
 
-        let de = parseGlobalDefaults(JSON.parse(globalDefaults));
-        let no = parseNodes(de, JSON.parse(nodesData));
-        let ed = parseEdges(de, JSON.parse(edgesData), no);
+        let gd = globalDefaults;
+        let nd = nodesData;
+        let ed = edgesData;
 
-        setNodes(no);
-        setEdges(ed);
+        if (language === "yaml") {
+            gd = yaml2json(globalDefaults);
+            nd = yaml2json(nodesData);
+            ed = yaml2json(edgesData);
+        }
+
+        let defaults = parseGlobalDefaults(JSON.parse(gd));
+        let nodes = parseNodes(defaults, JSON.parse(nd));
+        let edges = parseEdges(defaults, JSON.parse(ed), nodes);
+
+        setNodes(nodes);
+        setEdges(edges);
 
     }
 
@@ -76,16 +154,25 @@ const EdgesFlow = () => {
     function loadExample(e, number) {
         e.preventDefault();
 
-        let example = examples[number-1];
+        let example = examples[number - 1];
 
-        setGlobalDefaults(JSON.stringify(example[0]));
-        setNodesData(JSON.stringify(example[1]));
-        setEdgesData(JSON.stringify(example[2]));
+        let defaults = JSON.stringify(example[0]);
+        let nodes = JSON.stringify(example[1]);
+        let edges = JSON.stringify(example[2]);
+
+        if (language === "yaml") {
+            defaults = json2yaml(defaults);
+            nodes = json2yaml(nodes);
+            edges = json2yaml(edges);
+        }
+
+        setGlobalDefaults(defaults);
+        setNodesData(nodes);
+        setEdgesData(edges);
     }
 
     return (
         <>
-
             <div className="d-flex">
                 <Button className="primary" onClick={e => loadExample(e, 1)}>example 1</Button>
                 <Button className="primary" onClick={e => loadExample(e, 2)}>example 2</Button>
@@ -94,15 +181,25 @@ const EdgesFlow = () => {
                 <Button className="primary" onClick={e => loadExample(e, 5)}>example 5</Button>
             </div>
 
+            <Dropdown onSelect={changeLanguage}>
+                <Dropdown.Toggle variant="warning" id="dropdown-basic">
+                    Language: {language.toUpperCase()}
+                </Dropdown.Toggle>
+                <Dropdown.Menu>
+                    <Dropdown.Item eventKey="json" active>JSON</Dropdown.Item>
+                    <Dropdown.Item eventKey="yaml">YAML</Dropdown.Item>
+                </Dropdown.Menu>
+            </Dropdown>
+
             <div className="edit-area">
                 <div className="code-editor resizable" style={{height: "200px"}}>
                     <h5>Global defaults editor</h5>
                     <Editor
-                        defaultLanguage="json"
+                        language={language}
                         value={globalDefaults}
                         onChange={content => setGlobalDefaults(content)}
                         theme="vs-dark"
-                        style = {{
+                        style={{
                             width: "100%",
                             height: "100%",
                             minHeight: "250px",
@@ -111,16 +208,17 @@ const EdgesFlow = () => {
                     />
                 </div>
 
-                <div className="d-flex resizable code-editor" style={{height: "260px"}}/*style={{width: "97%", margin: "auto"}}*/>
+                <div className="d-flex resizable code-editor"
+                     style={{height: "350px"}}/*style={{width: "97%", margin: "auto"}}*/>
 
-                    <div className="node-edge-editor" >
+                    <div className="node-edge-editor">
                         <h5>Node editor</h5>
                         <Editor
-                            defaultLanguage="json"
+                            language={language}
                             value={nodesData}
-                            onChange={content=>setNodesData(content)}
+                            onChange={content => setNodesData(content)}
                             theme="vs-dark"
-                            style = {{
+                            style={{
                                 width: "100%",
                                 height: "100%",
                                 minHeight: "250px",
@@ -132,11 +230,11 @@ const EdgesFlow = () => {
                     <div className="node-edge-editor">
                         <h5>Edge editor</h5>
                         <Editor
-                            defaultLanguage="json"
+                            language={language}
                             value={edgesData}
                             onChange={content => setEdgesData(content)}
                             theme="vs-dark"
-                            style = {{
+                            style={{
                                 width: "100%",
                                 height: "100%",
                                 minHeight: "250px",
