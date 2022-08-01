ad8260dd0ad76cabb6757240d5745262df8df9ce
Tibo Stroo <tibo.stroo@hotmail.be>
2022-08-01 11:06:54 +0200

    Overflow hidden text editors

--- src/App.js
+++ src/App.js
@@ -226,9 +226,9 @@ const EdgesFlow = () => {
                         theme="vs-dark"
                         style={{
                             width: "100%",
-                            height: "100%",
-                            minHeight: "250px",
-                            margin: "auto"
+                            //height: "100%",
+                           // minHeight: "250px",
+                            //margin: "auto"
                         }}
                     />
                 </div>
@@ -245,9 +245,9 @@ const EdgesFlow = () => {
                             theme="vs-dark"
                             style={{
                                 width: "100%",
-                                height: "100%",
-                                minHeight: "250px",
-                                margin: "auto"
+                               // height: "100%",
+                                //minHeight: "250px",
+                                //margin: "auto"
                             }}
                         />
                     </div>
@@ -261,9 +261,9 @@ const EdgesFlow = () => {
                             theme="vs-dark"
                             style={{
                                 width: "100%",
-                                height: "100%",
-                                minHeight: "250px",
-                                margin: "auto"
+                                //height: "100%",
+                                //minHeight: "250px",
+                                //margin: "auto"
                             }}
                         />
 
