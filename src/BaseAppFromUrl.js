155ba9ef08dd6b21842a16a33a00b1bcf2527e5c
Tibo Stroo <tibo.stroo@hotmail.be>
2022-08-10 16:14:33 +0200

    Error handling when bad URL (#73)

--- src/BaseAppFromUrl.js
+++ src/BaseAppFromUrl.js
@@ -1,7 +1,7 @@
 import {useSearchParams} from "react-router-dom";
 import ReactFlow, {addEdge, Controls, useEdgesState, useNodesState} from "react-flow-renderer";
 import Node from "./components/node/Node";
-import {useCallback} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 
 const nodeTypes = {
@@ -10,38 +10,71 @@ const nodeTypes = {
 
 
 const Base = () => {
-    // use search params
-    // https://github.ugent.be/tistroo/webdev-project2-react/blob/main/project-react/src/components/genre/GenreDetail.js
 
     const [searchParams] = useSearchParams();
 
-    const [nodes, setNodes, onNodesChange] = useNodesState(JSON.parse(searchParams.get("nodes")));
-    const [edges, setEdges, onEdgesChange] = useEdgesState(JSON.parse(searchParams.get("edges")));
+    const [isError, setError] = useState(false);
+
+    const [nodes, setNodes, onNodesChange] = useNodesState([]);
+    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
 
     const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
 
-    console.log(JSON.parse(searchParams.get("nodes")))
+    useEffect(() => {
+        let nodes2json;
+        let edges2json;
+
+        try {
+            nodes2json = JSON.parse(searchParams.get("nodes"));
+            edges2json = JSON.parse(searchParams.get("edges"));
+        } catch (e) {
+            console.warn(e);
+            setError(true);
+            return;
+        }
+
+        setNodes(nodes2json);
+        setEdges(edges2json)
+
+
+    }, [searchParams, setEdges, setNodes])
 
 
     return <>
         <div style={{
-            height: window.innerHeight*0.9,
-            width: window.innerWidth*0.9,
+            height: window.innerHeight * 0.9,
+            width: window.innerWidth * 0.9,
             margin: "auto",
         }}>
-            <ReactFlow
-                nodes={nodes}
-                edges={edges}
-                onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                onConnect={onConnect}
-                snapToGrid={true}
-                nodeTypes={nodeTypes}
-                fitView
-                attributionPosition="top-right"
-            >
-                <Controls/></ReactFlow>
+            {!isError && Array.isArray(nodes) && nodes.length > 0
+                ? <ReactFlow
+                    nodes={nodes}
+                    edges={edges}
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgesChange}
+                    onConnect={onConnect}
+                    snapToGrid={true}
+                    nodeTypes={nodeTypes}
+                    fitView
+                    attributionPosition="top-right"
+                >
+                    <Controls/>
+                </ReactFlow>
+                : <div style={{
+                    width: "fit-content",
+                    height: "fit-content",
+                    position: "absolute",
+                    top: 0,
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    margin: "auto",
+                    textAlign: "center",
+                    fontSize: "1.5em"
+                }}>ERROR: Could not load diagram</div>
+            }
         </div>
+
     </>
 }
 
