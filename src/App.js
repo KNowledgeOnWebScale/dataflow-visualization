07aa888fb823ad1f47950e62f664b3355b0969a6
Tibo Stroo <tibo.stroo@hotmail.be>
2022-08-10 17:13:58 +0200

    Removed unused import (#73)

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import BaseApp from "./BaseApp";
 import BaseFromUrl from "./BaseAppFromUrl";
-import NotFound from "./components/NotFound";
+//import NotFound from "./components/NotFound";
 
 
 const EdgesFlow = () => {
