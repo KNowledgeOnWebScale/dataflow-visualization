b2f16dffe17124c06288352e60abee2b35046b1c
Tibo Stroo <tibo.stroo@hotmail.be>
2022-08-12 16:54:43 +0200

    Fix build error gh pages

--- src/BaseAppFromUrl.js
+++ src/BaseAppFromUrl.js
@@ -1,5 +1,5 @@
 import {useSearchParams} from "react-router-dom";
-import ReactFlow, {addEdge, Controls, useEdgesState, useNodesState} from "react-flow-renderer";
+import ReactFlow, {addEdge, useEdgesState, useNodesState} from "react-flow-renderer";
 import Node from "./components/node/Node";
 import {useCallback, useEffect, useState} from "react";
 import {setFlowData} from "./lib/setFlowData";
