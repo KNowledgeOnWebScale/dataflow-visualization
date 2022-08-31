caf34abbb4f1f74d4ed296adbc118e160bcdb976
Tibo Stroo <tibo.stroo@hotmail.be>
2022-08-31 13:06:22 +0200

    animated key is gone + some defaults for the animation key (fixes #6)

--- src/data/single-flow/examples/exampleData5.js
+++ src/data/single-flow/examples/exampleData5.js
@@ -1,14 +1,11 @@
-
 export const globalDefaultsJSON = {
     graph: {
         orientation: "vertical",
     },
-    node: {
-
-    },
+    node: {},
     edge: {
         strokeDasharray: "6 5",
-        animated: true,
+        animation: "default",
         type: "straight"
     }
 
@@ -16,38 +13,38 @@ export const globalDefaultsJSON = {
 
 export const nodesJSON = [
     {
-        shape:"comunica",
-        position: { x: 20, y: 90 },
+        shape: "comunica",
+        position: {x: 20, y: 90},
     },
     {
         // PARENTNODE
         id: 'solid',
         title: 'Solid Pod',
-        shape:"rectangle",
+        shape: "rectangle",
         strokeWidth: 5,
-        stroke:"#8370fb",
-        fill:"#12121212",
-        width:300,
+        stroke: "#8370fb",
+        fill: "#12121212",
+        width: 300,
         height: 100,
-        position: { x: 0, y: 200 },
+        position: {x: 0, y: 200},
     },
     {
         image: "solid",
         topText: "Solid",
         stroke: "#fff",
-        position: {x:-50, y:225}
+        position: {x: -50, y: 225}
     },
     {
         id: 'RDF-1',
-         label: 'RDF',
-       hgroup: "hgroup1",
-         position: { x: 20, y: 25 },
+        label: 'RDF',
+        hgroup: "hgroup1",
+        position: {x: 20, y: 25},
         parentNode: 'solid',
     },
     {
         id: 'RDF-2',
-         label: 'RDF',
-       hgroup: "hgroup1",
+        label: 'RDF',
+        hgroup: "hgroup1",
         parentNode: 'solid',
     },
     {
@@ -60,7 +57,7 @@ export const nodesJSON = [
     {
         id: 'RDF-4',
         label: 'RDF',
-        position: { x: 230, y: 25 },
+        position: {x: 230, y: 25},
         parentNode: 'solid',
     },
     {
@@ -68,17 +65,17 @@ export const nodesJSON = [
         position: {x: 95, y: 350}
     },
     {
-        id:"flickr",
+        id: "flickr",
         image: "https://cdn.worldvectorlogo.com/logos/flickr-1.svg",
         position: {x: 20, y: 450}
     },
     {
-        id:"imgur",
-        image:"https://cdn.worldvectorlogo.com/logos/imgur-logo.svg",
+        id: "imgur",
+        image: "https://cdn.worldvectorlogo.com/logos/imgur-logo.svg",
         position: {x: 95, y: 450}
     },
     {
-        id:"google",
+        id: "google",
         image: "https://cdn.cdnlogo.com/logos/g/82/google-g-2015.svg",
         position: {x: 170, y: 450}
     }
@@ -86,18 +83,18 @@ export const nodesJSON = [
 ];
 
 export const edgesJSON = [
-    { source: 'comunica', target: 'RDF-1' },
-    { source: 'comunica', target: 'RDF-2'},
-    { source: 'comunica', target: 'RDF-3' },
-    { source: 'comunica', target: 'RDF-4', type: "step", sourceHandle:"right", markerEnd: {type: "arrowclosed"} },
+    {source: 'comunica', target: 'RDF-1'},
+    {source: 'comunica', target: 'RDF-2'},
+    {source: 'comunica', target: 'RDF-3'},
+    {source: 'comunica', target: 'RDF-4', type: "step", sourceHandle: "right", markerEnd: {type: "arrowclosed"}},
 
-    { source: 'RDF-1', target: 'rmlio', zIndex:1 },
-    { source: 'RDF-2', target: 'rmlio', zIndex:1 },
-    { source: 'RDF-3', target: 'rmlio',zIndex:1 },
+    {source: 'RDF-1', target: 'rmlio', zIndex: 1},
+    {source: 'RDF-2', target: 'rmlio', zIndex: 1},
+    {source: 'RDF-3', target: 'rmlio', zIndex: 1},
 
-    { source: 'flickr', target: 'rmlio', animated: false},
-    { source: 'imgur', target: 'rmlio', animated:false},
-    { source: 'google', target: 'rmlio', animated:false},
+    {source: 'flickr', target: 'rmlio', animation: "none"},
+    {source: 'imgur', target: 'rmlio', animation: "none"},
+    {source: 'google', target: 'rmlio', animation: "none"},
 
 
 ];
